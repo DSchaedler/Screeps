51b33fad6fa39d4666cb5e454c069e845ef99ac5
DrakeShady <dominicschaedler@gmail.com>
2016-07-29 10:42:33 -0400

    Code Cleanup
    Removed a redundant if structure.

--- role.builder.js
+++ role.builder.js
@@ -5,14 +5,11 @@ var roleBuilder = {
         var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
 		targets.sort();
         if(targets.length > 0) {
-            if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(targets[0]);
-            }
-            creep.moveTo(targets[0]);
+			creep.moveTo(targets[0]);
+			creep.build(targets[0]);
         }
         else {
-            roleRepairer.run(creep);
-        }
+            roleRepairer.run(creep);}
     }
 };
 module.exports = roleBuilder;
