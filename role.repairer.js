1daf0c98e73b7d33285fafe75cbcfeb31f34d9b1
DrakeShady <dominicschaedler@gmail.com>
2016-07-29 11:06:43 -0400

    Removed redundant if structure

--- role.repairer.js
+++ role.repairer.js
@@ -6,9 +6,8 @@ var roleRepairer = {
 
 		targets.sort((a,b) => a.hits - b.hits);
 
-		if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
-			creep.moveTo(targets[0]);}
 		creep.moveTo(targets[0]);
+		creep.repair(targets[0])
 		if(targets.length == 0) {
 		    creep.moveTo(creep.room.controller);
             creep.upgradeController(creep.room.controller);}
