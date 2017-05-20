52d6ef0a8986e2ebc4fb7c5b8de7856d10aa5647
Dominic Schaedler <dominicschaedler@gmail.com>
2017-05-20 19:58:18 -0400

    missed a character

--- role.harvester.js
+++ role.harvester.js
@@ -7,7 +7,7 @@ var roleHarvester = {
             if(sources.length >0 && creep.harvest(sources[sourceNumber]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[sourceNumber]);}
         }
-        else if( (_(Game.creeps).filter( {memory: { role: 'mover' } } ).size() < 2 ) || (Memory.creep.source = '0') {
+        else if( (_(Game.creeps).filter( {memory: { role: 'mover' } } ).size() < 2 ) || (Memory.creep.source = '0')) {
             if (Game.spawns.Spawn1.energy < Game.spawns.Spawn1.energyCapacity) {
                 if(creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(Game.spawns.Spawn1);}
