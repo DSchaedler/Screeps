72fd1e793050dcee562fc9fc737a9ddcc14f0c09
Dominic Schaedler <dominicschaedler@gmail.com>
2017-05-20 18:52:48 -0400

    added back the type declaration

--- role.builder.js
+++ role.builder.js
@@ -3,7 +3,7 @@ var roleRepairer = require('role.repairer');
 var roleBuilder = {
     run: function(creep, loopCount) {
         if(creep.carry.energy != 0) {
-			var targets;
+			var targets = [];
 			if (loopCount >= 5 ){
 				var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
 				targets.sort();}
