17ef061b9da9804256c98969a27d9cd6ed049908
Dominic Schaedler <dominicschaedler@gmail.com>
2017-05-20 17:13:13 -0400

    fixed error

--- main.js
+++ main.js
@@ -49,11 +49,11 @@ module.exports.loop = function () {
     }
     else if(movers < harvesters) {
         Game.spawns.Spawn1.createCreep( [MOVE, MOVE, CARRY], null, { role: 'mover' } );}
-	else if(upgraders < floor(movers / 2)) {
+	else if(upgraders < Math.floor(movers / 2)) {
 		Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], null, { role: 'upgrader' } );}
     else if(builders < movers) {
         Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE, MOVE], null, { role: 'builder' } );}
-    else if(repairers < (floor(movers / 2)) && controllerLevel > 1) {
+    else if(repairers < (Math.floor(movers / 2)) && controllerLevel > 1) {
         Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE, MOVE], null, { role: 'repairer' } );}
     
     for(var name in Game.creeps) {
