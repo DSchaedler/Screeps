f1ec4be1bd0f216987cdeeb0d6bbcc623f3e44bd
Dominic Schaedler <dominicschaedler@gmail.com>
2017-05-20 15:09:25 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 require("lodash");
 
-var roleHarvester = require('role.harvester');
+require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 var roleRepairer = require('role.repairer');
