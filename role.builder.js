a0e795abd8bf57793fc796ff477767aaa351052d
Dominic Schaedler <dominicschaedler@gmail.com>
2017-05-20 18:23:30 -0400

    Fixed a statement

--- role.builder.js
+++ role.builder.js
@@ -4,17 +4,16 @@ var main = require('main');
 var roleBuilder = {
     run: function(creep) {
         if(creep.carry.energy != 0) {
-			if (main.loopCount >= 5{
+			if (main.loopCount >= 5 ){
 				var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-				targets.sort();
-				if(targets.length > 0) {
-					if (creep.build(targets[0]) == ERR_NOT_IN_RANGE){
-						creep.moveTo(targets[0]);
-					};
-				}
-				else {
-					roleRepairer.run(creep);}
+				targets.sort();}
+			if(targets.length > 0) {
+				if (creep.build(targets[0]) == ERR_NOT_IN_RANGE){
+					creep.moveTo(targets[0]);
+				};
 			}
+			else {
+				roleRepairer.run(creep);}
 		}	
     }
 };
