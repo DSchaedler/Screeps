cb46e0cb4df3cab83b35e7fb0a0f17d8ad283c3f
Dominic Schaedler <dominicschaedler@gmail.com>
2017-05-20 18:34:18 -0400

    testing

--- role.builder.js
+++ role.builder.js
@@ -3,14 +3,15 @@ var roleRepairer = require('role.repairer');
 var roleBuilder = {
     run: function(creep, loopCount) {
         if(creep.carry.energy != 0) {
-			var targets = []
+			var targets = [];
+			console.log(loopCount);
 			if (loopCount >= 5 ){
 				var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
 				targets.sort();}
+				
 			if(targets.length > 0) {
 				if (creep.build(targets[0]) == ERR_NOT_IN_RANGE){
-					creep.moveTo(targets[0]);
-				};
+					creep.moveTo(targets[0]);}
 			}
 			else {
 				roleRepairer.run(creep);}
