897fa6c11d97b3afcb7edb566c19eff22cdc982c
Dominic Schaedler <dominicschaedler@gmail.com>
2017-05-22 12:39:51 -0400

    reformmated, maybe actually fixed

--- role.repairer.js
+++ role.repairer.js
@@ -3,8 +3,11 @@ var roleRepairer = require('role.upgrader');
 var roleRepairer = {
     run: function(creep) {
         if(creep.carry.energy != 0) {
-			var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: function(object){return object.structureType === STRUCTURE_ROAD && (object.hits > object.hitsMax / 3);}})
-			};
+			var target = creep.pos.findClosest(FIND_STRUCTURES, {
+				filter: function(object){
+					return object.structureType !== STRUCTURE_CONTROLLER && (object.hits > object.hitsMax);
+				}
+			});
 
 			if (target != null) {
 				if (creep.repair(target) == ERR_NOT_IN_RANGE) {
