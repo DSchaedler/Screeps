f0511332e0ee5077276739fc024b4dfce2117624
Dominic Schaedler <dominicschaedler@gmail.com>
2017-05-21 21:15:47 -0400

    Make builders be repairers

--- role.builder.js
+++ role.builder.js
@@ -3,10 +3,15 @@ var roleRepairer = require('role.repairer');
 var roleBuilder = {
     run: function(creep, loopCount) {
         if(creep.carry.energy != 0) {
-			var target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-			if (creep.build(target) == ERR_NOT_IN_RANGE){
-				creep.moveTo(target);}
+			var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+			targets.sort();
+			if(targets.length > 0) {
+				if (creep.build(targets[0]) == ERR_NOT_IN_RANGE){
+					creep.moveTo(targets[0]);}
+			}
+			else {
+				roleRepairer.run(creep);}
 		}	
     }
-};
+}
 module.exports = roleBuilder;
