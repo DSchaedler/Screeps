f88ea00320a06d0331b676e85c28b805d4ad1155
Dominic Schaedler <dominicschaedler@gmail.com>
2017-05-22 08:41:23 -0400

    bug fix?

--- role.repairer.js
+++ role.repairer.js
@@ -1,7 +1,7 @@
 var roleRepairer = {
     run: function(creep) {
         if(creep.carry.energy != 0) {
-			var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {}
+			var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
 				filter: object => object.hits < object.hitsMax
 			});
 
