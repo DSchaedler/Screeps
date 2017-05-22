94193dc86950fc6e5ba184a7be7dc6cad8ce43f0
Dominic Schaedler <dominicschaedler@gmail.com>
2017-05-22 11:19:52 -0400

    added a bit to cache source locations

--- main.js
+++ main.js
@@ -15,6 +15,32 @@ const source1Points = 4;
 
 var loopCount = 0;
 
+//Save source IDs to memory - will allow for better code later.
+//Lets first add a shortcut prototype to the sources memory:
+Source.prototype.memory = undefined;
+
+for(var roomName in Game.rooms){//Loop through all rooms your creeps/structures are in
+    var room = Game.rooms[roomName];
+    if(!room.memory.sources){//If this room has no sources memory yet
+        room.memory.sources = {}; //Add it
+        var sources = room.find(FIND_SOURCES);//Find all sources in the current room
+        for(var i in sources){
+            var source = sources[i];
+            source.memory = room.memory.sources[source.id] = {}; //Create a new empty memory object for this source
+            //Now you can do anything you want to do with this source
+            //for example you could add a worker counter:
+            source.memory.workers = 0;
+        }
+    }else{ //The memory already exists so lets add a shortcut to the sources its memory
+        var sources = room.find(FIND_SOURCES);//Find all sources in the current room
+        for(var i in sources){
+            var source = sources[i];
+            source.memory = this.memory.sources[source.id]; //Set the shortcut
+        }
+    }
+}
+
+
 const profiler = require ('screeps-profiler');
 profiler.enable();
 
