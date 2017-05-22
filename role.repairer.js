74b29e3d1b3d4575ae9253458e967edbd9d1350a
Dominic Schaedler <dominicschaedler@gmail.com>
2017-05-22 12:34:18 -0400

    fix filter

--- role.repairer.js
+++ role.repairer.js
@@ -3,9 +3,8 @@ var roleRepairer = require('role.upgrader');
 var roleRepairer = {
     run: function(creep) {
         if(creep.carry.energy != 0) {
-			var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-				filter: object => (object.hits < object.hitsMax) && (object.structureType !== STRUCTURE_CONTROLLER)
-			});
+			var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: function(object){return object.structureType === STRUCTURE_ROAD && (object.hits > object.hitsMax / 3);}})
+			};
 
 			if (target != null) {
 				if (creep.repair(target) == ERR_NOT_IN_RANGE) {
