f3924488e09aa286c889324d0778dc1909d21ad0
Dominic Schaedler <dominicschaedler@gmail.com>
2017-05-21 21:21:40 -0400

    Revert "builders are broken"
    This reverts commit 23a853166e0520310b488c8ab8d14272beb416be.

--- role.builder.js
+++ role.builder.js
@@ -4,7 +4,7 @@ var roleBuilder = {
     run: function(creep, loopCount) {
         if(creep.carry.energy != 0) {
 			var targets = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-			if(targets) {
+			if(targets.length > 0) {
 				if (creep.build(targets[0]) == ERR_NOT_IN_RANGE){
 					creep.moveTo(targets[0]);}
 			}
