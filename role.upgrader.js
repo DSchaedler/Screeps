71efacbed75178676c85bc15a397a449dc099928
Dominic Schaedler <dominicschaedler@gmail.com>
2017-05-22 09:00:04 -0400

    slight CPU improvement on upgraders

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,9 +1,9 @@
 var roleUpgrader = {
     run: function(creep) {
         if(creep.carry.energy !=0) {
-			creep.moveTo(creep.room.controller);
-			creep.upgradeController(creep.room.controller);
+			if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+			creep.moveTo(creep.room.controller);}
 		}
     }
-};
+}
 module.exports = roleUpgrader;
