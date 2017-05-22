ba227c96d573e0435d1b59627361008d9b27fbfc
Dominic Schaedler <dominicschaedler@gmail.com>
2017-05-21 21:21:59 -0400

    Revert "Make builders be repairers"
    This reverts commit f0511332e0ee5077276739fc024b4dfce2117624.

--- role.builder.js
+++ role.builder.js
@@ -3,15 +3,10 @@ var roleRepairer = require('role.repairer');
 var roleBuilder = {
     run: function(creep, loopCount) {
         if(creep.carry.energy != 0) {
-			var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-			targets.sort();
-			if(targets.length > 0) {
-				if (creep.build(targets[0]) == ERR_NOT_IN_RANGE){
-					creep.moveTo(targets[0]);}
-			}
-			else {
-				roleRepairer.run(creep);}
+			var target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+			if (creep.build(target) == ERR_NOT_IN_RANGE){
+				creep.moveTo(target);}
 		}	
     }
-}
+};
 module.exports = roleBuilder;
