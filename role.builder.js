066bea0be12a9dae6cc8b121c6fd48407a260a95
Dominic Schaedler <dominicschaedler@gmail.com>
2017-05-21 21:17:18 -0400

    fix builders

--- role.builder.js
+++ role.builder.js
@@ -3,7 +3,7 @@ var roleRepairer = require('role.repairer');
 var roleBuilder = {
     run: function(creep, loopCount) {
         if(creep.carry.energy != 0) {
-			var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+			var targets = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
 			targets.sort();
 			if(targets.length > 0) {
 				if (creep.build(targets[0]) == ERR_NOT_IN_RANGE){
