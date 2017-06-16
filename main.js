3a62eb73f606afc7988eecdb7cb86a2d9019216c
Dominic Schaedler <dominicschaedler@gmail.com>
2017-06-16 15:31:40 -0400

    update source numbers
    apparently, i never did this. oops.

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ const roomID = 'W99S43';
 const roomControllerObject = Game.spawns.Spawn1.room.controller;
 const sources = Game.spawns.Spawn1.room.find(FIND_SOURCES);
 
-const source0Points = 1;
-const source1Points = 4;
+const source0Points = 4;
+const source1Points = 3;
 
 var loopCount = 0;
 
