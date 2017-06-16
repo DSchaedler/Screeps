bc754232421d07a64141c8dcd1295803ace05835
Dominic Schaedler <dominicschaedler@gmail.com>
2017-06-16 14:54:28 -0400

    Eh

--- role.mover.js
+++ role.mover.js
@@ -11,8 +11,8 @@ var roleMover = {
 				}
             }
 			if (harvesters.length > 0 ) {
-				harvesters.sort(function(a, b){return b.carry.energy - a.carry.energy})};
-				creep.moveTo(harvesters[0], {reusePath: 50}, {visualizePathStyle: {stroke: '#fff'}});
+				harvesters.sort(function(a, b){return b.carry.energy - a.carry.energy});
+				creep.moveTo(harvesters[0], {reusePath: 50}, {visualizePathStyle: {stroke: '#fff'}})};
             else {
                 creep.moveTo(3,25);}
         }
