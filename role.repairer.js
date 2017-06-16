bc8a666587b020966cde2fe3d81e26811b9c6b89
Dominic Schaedler <dominicschaedler@gmail.com>
2017-06-16 15:10:53 -0400

    directly rip someone elses code
    cuz why not

--- role.repairer.js
+++ role.repairer.js
@@ -3,32 +3,20 @@ var roleRepairer = {
 		loopCount = 0;
 		targets = [];
 		if(creep.carry.energy != 0) {
-			loopCount = loopCount + 1;
-			if(loopCount > 15) {
-				var fixThis = creep.room.find(FIND_STRUCTURES, {
-					filter: function(object){
-						if(oject.structureType == STRUCTURE_CONTROLLER){
-							return false;
-						}
-						if (object.hits > object.hitsMax - 1){
-							return false;
-						}
-						return true;
-				})}
-						
-				var targets = [fixThis];
-
-				targets.sort((a,b) => a.hits - b.hits);
-				loopCount = 0;
-			}
-
-			if (targets.length > 0) {
-				creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#fff'}});
-				creep.repair(targets[0]);}
-			else {
-				creep.moveTo(creep.room.controller);
-                creep.upgradeController(creep.room.controller);}
-		}
+			var SR = creep.pos.findClosest(FIND_STRUCTURES, {
+				filter: function(object){
+					if(object.structureType != STRUCTURE_WALL) {
+						return false;
+                    }
+                    if(object.hits > object.hitsMax / 3) {
+						return false;
+					}
+                    return true;
+                }
+            });     
+		creep.moveTo(SR);
+		creep.repair(SR);
+		} 
 	}
-};
+}
 module.exports = roleRepairer;
