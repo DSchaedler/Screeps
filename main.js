32c292534eeda71ceb933d9ba00b2860d659099b
Dominic Schaedler <dominicschaedler@gmail.com>
2017-06-16 17:32:05 -0400

    better tower repair code
    only repair structures if tower energy is greater than half

--- main.js
+++ main.js
@@ -96,7 +96,7 @@ module.exports.loop = function () {
 			else if (hurtCreeps.length > 0) {
 				towers.forEach(tower => tower.heal(hurtCreeps[0]));
 			}
-			else {
+			else if (towers[0].energy > towers[0].energyCapacity / 2) {
 				var targets = Game.rooms[roomID].find(FIND_STRUCTURES, { filter: object => object.hits < object.hitsMax	});
 				targets.sort((a,b) => a.hits - b.hits);
 		
