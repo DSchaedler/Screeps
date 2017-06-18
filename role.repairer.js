880d0bf25c66c4af9cd847cd57c124095e85ecd4
Dominic Schaedler <dominicschaedler@gmail.com>
2017-06-18 19:22:27 -0400

    fix

--- role.repairer.js
+++ role.repairer.js
@@ -18,9 +18,9 @@ var roleRepairer = {
 			
 			SR.sort(function(a,b) {return ( (a.hits + creep.pos.getRangeTo(a.pos.x, a.pos.y) ) - ( b.hits + creep.pos.getRangeTo(b.pos.x, b.pos.y) ) ) } );
 			
-			Game.rooms[roomID].visual.circle(SR[0].pos.x, SR[0].pos.y, {radius: 0.25, fill: '#0aa53b');
-			Game.rooms[roomID].visual.circle(SR[1].pos.x, SR[1].pos.y, {radius: 0.25, fill: '#c9c906');
-			Game.rooms[roomID].visual.circle(SR[2].pos.x, SR[2].pos.y, {radius: 0.25, fill: '#bd0000');
+			Game.rooms[roomID].visual.circle(SR[0].pos.x, SR[0].pos.y, {radius: 0.25, fill: '#0aa53b'});
+			Game.rooms[roomID].visual.circle(SR[1].pos.x, SR[1].pos.y, {radius: 0.25, fill: '#c9c906'});
+			Game.rooms[roomID].visual.circle(SR[2].pos.x, SR[2].pos.y, {radius: 0.25, fill: '#bd0000'});
 			
 			creep.moveTo(SR[0], {visualizePathStyle: {stroke: '#ee9c00'}});
 			creep.repair(SR[0]);
