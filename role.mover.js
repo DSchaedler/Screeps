5c0345eaa8c4f1dbb57a95c468175b475a408d72
Dominic Schaedler <dominicschaedler@gmail.com>
2017-06-18 15:06:56 -0400

    creeps refil energy if less than 5%
    We've been having creeps with almost no energy waste time trying to
    deliver it

--- role.mover.js
+++ role.mover.js
@@ -2,7 +2,7 @@ require('lodash');
 
 var roleMover = {
     run: function(creep, source) {
-        if(creep.carry.energy == 0) {
+        if(creep.carry.energy < (creep.carryCapacity /20)) {
             var harvesters = [];
             for(var name in Game.creeps) {
                 var currentCreep = Game.creeps[name];
