f15ddb4c0d1c6bdb19aca147bb4a23d07232462f
Dominic Schaedler <dominicschaedler@gmail.com>
2017-06-18 19:18:32 -0400

    fix

--- role.repairer.js
+++ role.repairer.js
@@ -18,9 +18,9 @@ var roleRepairer = {
 			
 			SR.sort(function(a,b) {return ( (a.hits + creep.pos.getRangeTo(a.pos.x, a.pos.y) ) - ( b.hits + creep.pos.getRangeTo(b.pos.x, b.pos.y) ) ) } );
 			
-			Game.rooms[roomid].visual.circle(SR[0].pos.x, SR[0].pos.y);
-			Game.rooms[roomid].visual.circle(SR[1].pos.x, SR[1].pos.y);
-			Game.rooms[roomid].visual.circle(SR[2].pos.x, SR[2].pos.y);
+			Game.rooms[roomID].visual.circle(SR[0].pos.x, SR[0].pos.y);
+			Game.rooms[roomID].visual.circle(SR[1].pos.x, SR[1].pos.y);
+			Game.rooms[roomID].visual.circle(SR[2].pos.x, SR[2].pos.y);
 			
 			creep.moveTo(SR[0], {visualizePathStyle: {stroke: '#ee9c00'}});
 			creep.repair(SR[0]);
