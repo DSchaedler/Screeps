7538b3af3d33b7c83ee31f9f0e23a52b21b5f643
Dominic Schaedler <dominicschaedler@gmail.com>
2017-06-18 14:50:31 -0400

    had to swap for percentages to work

--- role.repairer.js
+++ role.repairer.js
@@ -13,7 +13,7 @@ var roleRepairer = {
                 }
             });
 			
-			SR.sort(function(a,b) {return ((a.hits / a.hitsMax) - (b.hits / b.hitsMaz))});
+			SR.sort(function(a,b) {return ((b.hits / b.hitsMax) - (a.hits / a.hitsMax))});
 			
 			creep.moveTo(SR[0]);
 			creep.repair(SR[0]);
