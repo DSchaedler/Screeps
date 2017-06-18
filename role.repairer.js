b1fea9233a015329ed288cf5fa7ee39c338e7573
Dominic Schaedler <dominicschaedler@gmail.com>
2017-06-18 19:06:02 -0400

    Impliment range based repairing
    add the distance from repairer to target to make farther objects seem
    more repaired

--- role.repairer.js
+++ role.repairer.js
@@ -6,14 +6,20 @@ var roleRepairer = {
 					if(object.structureType != STRUCTURE_WALL) {
 						return false;
                     }
-                    if(object.hits > object.hitsMax) {
+                    if(object.hits >= object.hitsMax) {
 						return false;
 					}
                     return true;
                 }
             });
 			
-			SR.sort(function(a,b) {return ( (a.hits / a.hitsMax) - (b.hits  / b.hitsMax) ) } );
+			SR.sort(function(a,b) {return ( (a.hits + creep.pos.getRangeTo(a.pos.x, a.pos.y) ) - ( b.hits + creep.pos.getRangeTo(b.pos.x, b.pos.y) ) ) } );
+			
+			try {
+			    console.log("Repair Target Values" + (SR[0].hits + creep.pos.getRangeTo(SR[0].pos.x, SR[0].pos.y) ) + ',' + ( SR[1].hits + creep.pos.getRangeTo(SR[1].pos.x, SR[1].pos.y) ) + ',' + ( SR[2].hits + creep.pos.getRangeTo(SR[2].pos.x, SR[2].pos.y) ));
+			} catch(err){
+			    console.log(err);
+			}
 			
 			creep.moveTo(SR[0], {visualizePathStyle: {stroke: '#ee9c00'}});
 			creep.repair(SR[0]);
