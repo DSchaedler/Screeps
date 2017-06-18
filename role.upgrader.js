a2157936b0e0ee4680648f307522cf0af69606c7
Dominic Schaedler <dominicschaedler@gmail.com>
2017-06-18 17:47:30 -0400

    different visuals for different tasks

--- role.upgrader.js
+++ role.upgrader.js
@@ -2,7 +2,7 @@ var roleUpgrader = {
     run: function(creep) {
         if(creep.carry.energy !=0) {
 			creep.upgradeController(creep.room.controller);
-			creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#fff'}});
+			creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#48c906'}});
 		}
     }
 }
