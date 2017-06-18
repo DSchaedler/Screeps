dcb858a91898261a7f72be9b5d6c02bcc5b944e0
Dominic Schaedler <dominicschaedler@gmail.com>
2017-06-18 19:07:18 -0400

    fix formatting

--- role.repairer.js
+++ role.repairer.js
@@ -16,7 +16,7 @@ var roleRepairer = {
 			SR.sort(function(a,b) {return ( (a.hits + creep.pos.getRangeTo(a.pos.x, a.pos.y) ) - ( b.hits + creep.pos.getRangeTo(b.pos.x, b.pos.y) ) ) } );
 			
 			try {
-			    console.log("Repair Target Values" + (SR[0].hits + creep.pos.getRangeTo(SR[0].pos.x, SR[0].pos.y) ) + ',' + ( SR[1].hits + creep.pos.getRangeTo(SR[1].pos.x, SR[1].pos.y) ) + ',' + ( SR[2].hits + creep.pos.getRangeTo(SR[2].pos.x, SR[2].pos.y) ));
+			    console.log("Repair Target Values: " + (SR[0].hits + creep.pos.getRangeTo(SR[0].pos.x, SR[0].pos.y) ) + ', ' + ( SR[1].hits + creep.pos.getRangeTo(SR[1].pos.x, SR[1].pos.y) ) + ', ' + ( SR[2].hits + creep.pos.getRangeTo(SR[2].pos.x, SR[2].pos.y) ));
 			} catch(err){
 			    console.log(err);
 			}
