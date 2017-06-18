921a231149f0d6f627a5fec60b6f74e9c333f398
Dominic Schaedler <dominicschaedler@gmail.com>
2017-06-18 14:47:16 -0400

    % based repair system

--- role.repairer.js
+++ role.repairer.js
@@ -13,7 +13,7 @@ var roleRepairer = {
                 }
             });
 			
-			SR.sort(function(a,b) {return a.hits - b.hits});
+			SR.sort(function(a,b) {return ((a.hits / a.hitsMax) - (b.hits / b.hitsMaz))});
 			
 			creep.moveTo(SR[0]);
 			creep.repair(SR[0]);
