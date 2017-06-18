fc663800986c2be983d903d1b6c42334f00da7c4
Dominic Schaedler <dominicschaedler@gmail.com>
2017-06-18 19:12:27 -0400

    change log output to circles

--- role.repairer.js
+++ role.repairer.js
@@ -15,11 +15,9 @@ var roleRepairer = {
 			
 			SR.sort(function(a,b) {return ( (a.hits + creep.pos.getRangeTo(a.pos.x, a.pos.y) ) - ( b.hits + creep.pos.getRangeTo(b.pos.x, b.pos.y) ) ) } );
 			
-			try {
-			    console.log("Repair Target Values: " + (SR[0].hits + creep.pos.getRangeTo(SR[0].pos.x, SR[0].pos.y) ) + ', ' + ( SR[1].hits + creep.pos.getRangeTo(SR[1].pos.x, SR[1].pos.y) ) + ', ' + ( SR[2].hits + creep.pos.getRangeTo(SR[2].pos.x, SR[2].pos.y) ));
-			} catch(err){
-			    console.log(err);
-			}
+			RoomVisual.circle(SR[0].pos.x, SR[0].pos.y);
+			RoomVisual.circle(SR[1].pos.x, SR[1].pos.y);
+			RoomVisual.circle(SR[2].pos.x, SR[2].pos.y);
 			
 			creep.moveTo(SR[0], {visualizePathStyle: {stroke: '#ee9c00'}});
 			creep.repair(SR[0]);
