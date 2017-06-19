b4ebcf2ad052004a8a06ad45f6b4fe5aef8b6246
Dominic Schaedler <dominicschaedler@gmail.com>
2017-06-19 10:40:02 -0400

    remove the extra builder
    redundant with the new model

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ module.exports.loop = function () {
 				Game.spawns.Spawn1.createCreep( [MOVE, MOVE, CARRY, CARRY], null, {role: 'mover', source: '1'} );}}
 		else if(upgraders < 2) {
 			Game.spawns.Spawn1.createCreep( [WORK, CARRY, CARRY, CARRY, MOVE], null, { role: 'upgrader' } );}
-		else if(builders < 2) {
+		else if(builders < 1) {
 			Game.spawns.Spawn1.createCreep( [WORK, CARRY, CARRY, MOVE, MOVE], null, { role: 'builder' } );}
 		else if((repairers < 2 && controllerLevel <4 ) || (repairers < 1 && controllerLevel >= 4)) {
 			Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE, MOVE], null, { role: 'repairer' } );}
